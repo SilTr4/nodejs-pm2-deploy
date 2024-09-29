31b0890daf81faf571729a711dcdb6b49f03679a
MAG_It_student <tc4evlad@yandex.ru>
2024-09-30 01:31:46 +0300

    front deploy settings

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,26 @@
+require('dotenv').config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, FILE, DEPLOY_HOST, DEPLOY_PATH, KEY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'api-service',
+    script: './dist/app.js',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      key: KEY_PATH,
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/SilTr4/nodejs-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ${FILE} ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/frontend`,
+      'post-deploy': 'cd frontend && npm i && npm update && npm audit fix --force && npm run build',
+    },
+  },
+};
